22c7e4e83b2cab54b45d73e8beee823c07503d6f
Kyle <kfalicov@gmail.com>
2019-07-20 00:32:03 -0400

    panel tilt and auto redirect

--- load_panels.js
+++ load_panels.js
@@ -1,17 +1,25 @@
 const e = React.createElement;
 const domContainer = document.querySelector('#boxes_container');
 
+let div;
 var projs = firebase.database().ref('projects');
 projs.once('value').then(function(snapshot){
     snapshot.forEach(function(child){
-        let div = document.createElement('div');
+        div = document.createElement('div');
         domContainer.appendChild(div);
-        ReactDOM.render(<div className='zoom' data-tilt data-tilt-reverse data-tilt-full-page-listening>{child.val().name}</div>, div);
+        ReactDOM.render(<div className='zoom'>
+            <div className='title'>{child.val().name}</div>
+            <div className='subtitle'>{child.val().desc}</div>
+            <a href={child.val().url}></a>
+        </div>, div);
         
+        VanillaTilt.init(div, {
+            reverse:                true,
+            perspective:            1000,
+            reset:                  false, 
+            "full-page-listening":  true
+        });
     });
-    VanillaTilt.init(document.querySelectorAll(".zoom"), {
-        reverse:                true,
-        "full-page-listening":  true
-    });
+    
 });
 
